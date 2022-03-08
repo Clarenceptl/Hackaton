cacfa32346bf544a35aabf086d63eae72ebacd02
Clarence Potel <potel.clarence@gmail.com>
2022-03-08 22:29:30 +0100

    début translation

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -5,13 +5,33 @@ namespace App\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'default')]
+    public function __construct(private TranslatorInterface $translator){}
+
+    #[Route('/{_locale}', name: 'default', requirements:[ "_locale"=>"en|fr|de"])]
     public function index(): Response
     {
-        return $this->render('front/default/index.html.twig', [
+        dd($this->translator->trans('road.home'));
+        return $this->render('front/default/home.html.twig', [
+            'controller_name' => 'FRONT'
+        ]);
+    }
+
+    #[Route('/whatwedo', name: 'page_whatwedo')]
+    public function showPageWhatWeDo(): Response
+    {
+        return $this->render('front/default/whatwedo.html.twig', [
+            'controller_name' => 'FRONT',
+        ]);
+    }
+
+    #[Route('/', name: 'page_whatweare')]
+    public function showPageWhatWeAre(): Response
+    {
+        return $this->render('front/default/whatwedo.html.twig', [
             'controller_name' => 'FRONT',
         ]);
     }
