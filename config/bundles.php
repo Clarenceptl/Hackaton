4063d343077efb1b83d8146a3899bd8e915d464f
victor.valee <valee.victor@gmail.com>
2022-03-11 12:28:37 +0100

    easyadmin deleted

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    // EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
